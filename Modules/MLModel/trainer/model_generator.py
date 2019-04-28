2046d818dc70e855113bb7f2bc17e4bb1809f29d
Matt Weber <mawe6437@colorado.edu>
2019-04-28 14:38:05 -0600

    Clean up ML Model code

--- Modules/MLModel/trainer/model_generator.py
+++ Modules/MLModel/trainer/model_generator.py
@@ -14,65 +14,66 @@ import argparse
 
 def model(job_dir):
 
-    hidden_size = 300
-    embedding_size = 300
-    embedding_file = 'glove.840B.300d.txt'
+    # Create our dual encoder LSTM model
+    hidden_sz = 300
+    emb_sz = 300
+    emb_file = 'glove.840B.300d.txt'
 
     print('Now indexing word vectors...')
 
-    # Load the embedding vectors
-    embeddings_index = {}
-    with file_io.FileIO(job_dir + embedding_file, mode='r') as f:
+    # Create the embedding vectors
+    emb_idx = {}
+    with file_io.FileIO(job_dir + emb_file, mode='r') as f:
         for line in f:
-            values = line.split()
-            word = values[0]
+            val = line.split()
+            word = val[0]
             try:
-                coefs = np.asarray(values[1:], dtype='float32')
+                wd_vec = np.asarray(values[1:], dtype='float32')
             except ValueError:
              continue
-            embeddings_index[word] = coefs
+            emb_idx[word] = wd_vec
    
     # Load the parameters of our tokenized words from the train/dev/test set (keras preprocessed)
-    MAX_SEQUENCE_LENGTH, MAX_NB_WORDS, word_index = pickle.load(file_io.FileIO(job_dir + 'params.pkl', mode="r"))
+    max_seq_len, max_word_len, word_idx = pickle.load(file_io.FileIO(job_dir + 'params.pkl', mode="r"))
 
-    print("MAX_SEQUENCE_LENGTH: {}".format(MAX_SEQUENCE_LENGTH))
-    print("MAX_NB_WORDS: {}".format(MAX_NB_WORDS))
-    
-    print("Now loading embedding matrix...")
-    num_words = min(MAX_NB_WORDS, len(word_index)) + 1
-    embedding_matrix = np.zeros((num_words , embedding_size))
-    for word, i in word_index.items():
-        if i >= MAX_NB_WORDS:
+    print("Now generating embedding vector...")
+    num_words = min(max_word_len, len(word_idx)) + 1
+    emb_vec = np.zeros((num_words , emb_sz))
+    for word, i in word_idx.items():
+        if i >= max_word_len:
             continue
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
+        emb = emb_idx.get(word)
+        if emb is not None:
             # words not found in embedding index will be all-zeros.
-            embedding_matrix[i] = embedding_vector
+            emb_vec[i] = emb
 
     print("Now building dual encoder lstm model...")
-    # define lstm encoder
+    # Define the lstm encoder
     encoder = Sequential()
-    encoder.add(Embedding(output_dim=embedding_size,
-                            input_dim=MAX_NB_WORDS,
-                            input_length=MAX_SEQUENCE_LENGTH,
-                            weights=[embedding_matrix],
-                            mask_zero=True,
-                            trainable=True))
-    
-    encoder.add(LSTM(units=hidden_size))
-    
-    context_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-    response_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    encoder.add(Embedding(output_dim=emb_sz,
+                          input_dim=max_word_len,
+                          input_length=max_seq_len,
+                          weights=[emb_vec],
+                          mask_zero=True,
+                          trainable=True))
+   
+    # Add LSTM Layer
+    encoder.add(LSTM(units=hidden_sz))
+   
+    # Define our inputs - both context and response sequence
+    context_input = Input(shape=(max_seq_len,), dtype='int32')
+    response_input = Input(shape=(max_seq_len,), dtype='int32')
 
+    # Encode them
     context_branch = encoder(context_input)
     response_branch = encoder(response_input)
-    
-    concatenated = multiply([context_branch, response_branch])
-    out = Dense((1), activation = "sigmoid") (concatenated)
+   
+    # Multiply these together and add as the output
+    concat = multiply([context_branch, response_branch])
+    out = Dense((1), activation = "sigmoid") (concat)
 
+    # Create the model
     model = Model([context_input, response_input], out)
-
-    print(encoder.summary())
 
     return model
 
@@ -108,16 +109,12 @@ def main(job_dir,**args):
     tst_ctxt, tst_rsp, tst_lbl = pickle.load(file_io.FileIO(job_dir + 'test.pkl', mode="r"))
     dev_ctxt, dev_rsp, dev_lbl = pickle.load(file_io.FileIO(job_dir + 'dev.pkl', mode="r"))
     
-    #print('%s train entries.' % len(tr_ctxt))
-    #print('%s dev entries.' % len(dev_ctxt))
-    #print('%s test entries.' % len(tst_ctxt))
-    
     # Adding the TensorBoard callback
-    tensorboard = callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+    tsboard = callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
     
     print("Training the model...")
     ucantoo_model.fit([tr_ctxt, tr_rsp], tr_lbl,
-        batch_size=bat_sz, epochs=eps, callbacks=[tensorboard],
+        batch_size=bat_sz, epochs=eps, callbacks=[tsboard],
         validation_data=([dev_ctxt, dev_rsp], dev_lbl), verbose=1)
 
     # classify the test set
